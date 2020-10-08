87a68dd5881aad7bc442d39006e0a31c70da9f9f
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-08 23:48:45 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+
+  attr_accessor :name, :appointment
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    Appointment.all.map do |appointment|
+      appointment.patient
+    end
+    
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+end 
