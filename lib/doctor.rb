a3a8471f00dcad17e9d529a5f11b072fec902e00
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-09 00:21:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,9 +13,7 @@ class Doctor
   end
 
   def patients
-    appointments.map do |appointment|
-      appointment.patient
-    end
+    appointments.collect {|appointment| appointment.patient}
   end
 
   def self.all
